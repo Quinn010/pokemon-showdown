5bd6f5db6a5c8974b69f04f27faae8591489f2bd
Quinn010 <quinnwilk@gmail.com>
2023-05-17 15:54:04 +0200

    Create abilities.ts

---
+++ data/mods/mmo/abilities.ts
@@ -0,0 +1,61 @@
+/*
+
+Ratings and how they work:
+
+-1: Detrimental
+	  An ability that severely harms the user.
+	ex. Defeatist, Slow Start
+
+ 0: Useless
+	  An ability with no overall benefit in a singles battle.
+	ex. Color Change, Plus
+
+ 1: Ineffective
+	  An ability that has minimal effect or is only useful in niche situations.
+	ex. Light Metal, Suction Cups
+
+ 2: Useful
+	  An ability that can be generally useful.
+	ex. Flame Body, Overcoat
+
+ 3: Effective
+	  An ability with a strong effect on the user or foe.
+	ex. Chlorophyll, Sturdy
+
+ 4: Very useful
+	  One of the more popular abilities. It requires minimal support to be effective.
+	ex. Adaptability, Magic Bounce
+
+ 5: Essential
+	  The sort of ability that defines metagames.
+	ex. Imposter, Shadow Tag
+
+*/
+
+export const Abilities: {[abilityid: string]: AbilityData} = {
+	noability: {
+		isNonstandard: "Past",
+		name: "No Ability",
+		rating: 0.1,
+		num: 0,
+},
+slushrush: {
+	onModifySpe(spe, pokemon) {
+		if (this.field.isWeather(['hail', 'snow'])) {
+			return this.chainModify(2);
+		}
+	},
+	name: "Slush Rush",
+	rating: 3,
+	num: 202,
+
+},
+snowwarning: {
+	onStart(source) {
+		this.field.setWeather('snow');
+	},
+	name: "Snow Warning",
+	rating: 4,
+	num: 117,
+},
+}
